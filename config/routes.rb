5e3722a699a6a605529fc890ac1dd2f8abc0d763
Kit <dev_kit@icloud.com>
2015-05-28 10:29:59 -0600

    New order button creates a new order and take the user to the order show page

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
 
   resources :items,  only: [:index, :show]
   resources :orders, only: [:index, :show]
+    post "orders", to: "order#create", as: "create_order"
   resources :users,  only: [:index, :show]
 end
